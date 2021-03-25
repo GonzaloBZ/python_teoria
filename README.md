82e4820069dd3b4c91b307924bf6dd61b3403a54
Gonza BZ <gonzalo.h.bernardez@gmail.com>
2021-03-25 20:18:12 -0300

    Agrego primer archivo de un desafio

---
+++ README.md
@@ -0,0 +1,3 @@
+# python_teoria
+
+Éste repositorio contendrá los ejercicios de teoría del Seminario de Python.
---
+++ clase1/run.py
@@ -0,0 +1,10 @@
+# Dadas dos cadenas ingresadas desde el teclado, imprimir aquella que tenga más caracteres.
+
+cad_1 = input("Ingresa una cadena de caracteres: ")
+cad_2 = input("Ingresa otra cadena de caracteres: ")
+    if len(cad_1) > len(cad_2):
+        print(f"La cadena {cad_1} es más larga.")
+    elif len(cad_1) == len(cad_2):
+        print("Las cadenas tienen la misma longitud.")
+    else:
+        print(f"La cadena {cad_2} es más larga.")
